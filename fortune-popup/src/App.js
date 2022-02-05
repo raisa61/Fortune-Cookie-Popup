9f9aa9b1e4092bec28dbcad201c5e7e8b24f1cd9
Raisa Zaman <raisazaman2000@gmail.com>
2022-02-05 11:52:22 -0500

    visibility works, need to add style

--- fortune-popup/src/App.js
+++ fortune-popup/src/App.js
@@ -15,6 +15,7 @@ function App() {
         console.log(res.data); //logging it into the console to check if correct data is being pulled
         const fortuneLength = res.data.length; //grabbing the length of the array
         //console.log(fortuneLength);
+
         setFortune(res.data[Math.floor(Math.random() * fortuneLength)]);
       })
       .catch((err) => {
@@ -22,12 +23,15 @@ function App() {
       });
   }
 
+  const [clicked, setClicked] = useState(false);
+
   //change image on fortune cookie click
   const clickHandler = (e) => {
     e.target.setAttribute(
       "src",
       "https://i.ibb.co/cksx7kr/kisspng-fortune-cookie-drawing-clip-art-fortune-cookie-5b237469209879-9079770415290502171335.png"
     );
+    setClicked(true);
   };
 
   return (
@@ -38,7 +42,12 @@ function App() {
           src="https://i.ibb.co/9YQV2qq/kisspng-fortune-cookie-biscuits-bakery-drawing-clip-art-fortune-cookies-5b0ec5e3013c23-3980054715276.png"
         />
       </button>
-      <div className="fortuneMessage">{fortune}</div>
+      <div
+        className="fortuneMessage"
+        style={{ visibility: clicked ? "visible" : "hidden" }}
+      >
+        {fortune}
+      </div>
     </div>
   );
 }
