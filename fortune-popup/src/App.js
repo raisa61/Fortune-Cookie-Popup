71bf5899e840aab56d5e756b585d5f71cb22fef1
Raisa Zaman <raisazaman2000@gmail.com>
2022-02-05 10:11:20 -0500

    reading from json works

--- fortune-popup/src/App.js
+++ fortune-popup/src/App.js
@@ -1,23 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const fortuneMessages = [
-    "A fresh start will put you on your way.",
-    "A hunch is creativity trying to tell you something.",
-    "A good time to finish up old tasks.",
-  ];
-
   const [fortune, setFortune] = useState("");
 
+  useEffect(() => {
+    fetchFortunes();
+  }, []);
+  function fetchFortunes() {
+    axios
+      .get("./data.json") //returns a promise
+      .then((res) => {
+        console.log(res.data); //logging it into the console to check if correct data is being pulled
+        const fortuneLength = res.data.length; //grabbing the length of the array
+        //console.log(fortuneLength);
+        setFortune(res.data[Math.floor(Math.random() * fortuneLength)]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  //change image on fortune cookie click
   const clickHandler = (e) => {
     e.target.setAttribute(
       "src",
       "https://i.ibb.co/cksx7kr/kisspng-fortune-cookie-drawing-clip-art-fortune-cookie-5b237469209879-9079770415290502171335.png"
     );
-    const fortuneLength = fortuneMessages.length;
-    setFortune(fortuneMessages[Math.floor(Math.random() * fortuneLength)]);
   };
+
   return (
     <div className="App">
       <button onClick={clickHandler}>
@@ -26,7 +38,7 @@ function App() {
           src="https://i.ibb.co/9YQV2qq/kisspng-fortune-cookie-biscuits-bakery-drawing-clip-art-fortune-cookies-5b0ec5e3013c23-3980054715276.png"
         />
       </button>
-      <div>{fortune}</div>
+      <div className="fortuneMessage">{fortune}</div>
     </div>
   );
 }
